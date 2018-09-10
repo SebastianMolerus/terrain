7e18767a8ee6eb7c92681e9117f93b3bee814c65
mojPC-Komputer\mojPC <sebastian.molerus@gmail.com>
2018-09-11 00:20:13 +0200

    fun with 3d

--- my_terrain/Main.cpp
+++ my_terrain/Main.cpp
@@ -8,6 +8,7 @@
 #include <time.h>  
 #include <sstream>
 #include "Shader.h"
+#include "stb_image.h"
 
 
 #define _WINDOW_WIDTH 1024
@@ -40,7 +41,7 @@ void setfpstitle(GLFWwindow* window, string title, float fElapsed)
 {
 	stringstream ss;
 	ss << title << ". ";
-	ss << (int)(1.0f / fElapsed);	
+	ss << (int)(1.0f / fElapsed);
 	ss << " FPS.";
 	glfwSetWindowTitle(window, ss.str().c_str());
 }
@@ -53,7 +54,7 @@ void howmanyatribs()
 }
 
 int main()
-{	
+{
 	InitializeGLFW();
 
 	GLFWwindow* window = glfwCreateWindow(_WINDOW_WIDTH, _WINDOW_HEIGHT, "Ruslemo terrain", NULL, NULL);
@@ -75,17 +76,20 @@ int main()
 	glfwSetFramebufferSizeCallback(window, resize_callback);
 
 	// Z-BUFFER
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
 	//=========================================================================================
 
 	Shader myShader("vertex.txt", "fragment.txt");
-	
+
 
 	float verticles[]{
-		 -0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 	};
 
 	unsigned int VAO, VBO, EBO;
@@ -97,9 +101,46 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticles), verticles, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Object Positions
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	// Texture positions
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	//=========================================================================================
+	// TEXTURES
+
+	unsigned int grass_texture;
+	glGenTextures(1, &grass_texture);
+
+	glBindTexture(GL_TEXTURE_2D, grass_texture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("grass.jpg", &width, &height, &nrChannels, STBI_rgb_alpha);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cerr << "Failed to load texture " << endl;
+	}
+	stbi_image_free(data);
+
+
+	glActiveTexture(GL_TEXTURE0);
+	myShader.Use();
+	myShader.setInt("texture1", 0); // connect sampler in fragment shader with active texture ( GL_TEXTURE0 );
 
 	//=========================================================================================
 	// MAIN GAME LOOP
@@ -108,9 +149,11 @@ int main()
 		// check exit state
 		escape(window);
 
-		glClearColor(0.754f, 0.754f, 0.753f, 1.0f); 
-		glClear(GL_COLOR_BUFFER_BIT); 
-		myShader.Use();
+
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 
 		// set fps title
 		float fCurrentFrame = (float)glfwGetTime();
@@ -118,11 +161,24 @@ int main()
 		fLastFrame = fCurrentFrame;
 		setfpstitle(window, "Ruslemo terrain", fElapsed);
 
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
 
+		projection = glm::perspective(glm::radians(45.0f), (float)_WINDOW_WIDTH / _WINDOW_HEIGHT, 0.1f, 100.0f);
+		glm::vec3 cameraPos   = glm::vec3(0, sinf(glfwGetTime()) + 3, 3.0f );
+		glm::vec3 cameraFront = glm::vec3(0, -1.0f, -1.0f);
+		glm::vec3 cameraUp    = glm::vec3(0, sinf(glfwGetTime()), -1.0f);
 
-		
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+		model = glm::rotate(model, glm::radians((float)glfwGetTime()), glm::vec3(3.0f, 3.0f, 3.0f));
+
+		myShader.setMat4("model", model);
+		myShader.setMat4("view", view);
+		myShader.setMat4("projection", projection);
 		//glUniform3fv(glGetUniformLocation(myShader.m_nID, "Color"), sizeof(color), glm::value_ptr(color));
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
